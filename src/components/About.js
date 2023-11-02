718244b12d001059b0c037e4c3fc80a20f75c4c2
mgonzalez0825 <manuggq@icloud.com>
2023-11-02 19:52:53 -0400

    fix resume link

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ const About = () => {
               <i> <FontAwesomeIcon icon={faGithub} size="xl" /></i>
                 GitHub
               </a>
-              <a href="../downloads/MannyGonzalezResume.pdf" download="MannyGonzalezResume.pdf">
+              <a href="/src/downloads/MannyGonzalezResume.pdf" download="MannyGonzalezResume.pdf">
               <i><FontAwesomeIcon icon={faDownload} size="xl" /></i>  
                 Download Resume
               </a>
